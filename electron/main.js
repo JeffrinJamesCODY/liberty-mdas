4357dd5e55aedb5d89d287779214ecf50d16caec
Jeffrin James <jeffrinjamesworks@gmail.com>
2026-02-28 00:03:01 +0000

    main.js version#1 (electron)

--- electron/main.js
+++ electron/main.js
@@ -10,6 +10,48 @@ function createWindow () {
 
         titleBarStyle: 'hiddenInset',
         frame : false,
-        
+        transparent : false,
+        backgroundColour: '#000408',
+
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            webSecurity: true,
+
+            allowRunningInsecureContent: false,
+
+        },
+
+        title: 'LIBERTY-MDAS - Multi-Domain Awareness Suite',
+        show: false,
+    })
+    
+    win.once('ready-to-show', () => {
+        win.show()
+        win.maximize()
+    })
+    if (isDev) {
+        win.loadURL('http://localhost:3000')
+        win.webContents.openDevTools({mode: 'detach'})
+
+    } else {
+        win.loadFile(path.join(__dirname, '../build/index.html'))
+    }
+
+    win.webContents.setWindowOpenHandler(({ url }) => {
+        shell.openExternal(url)
+        return { action: 'deny' }
     })
 }
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
