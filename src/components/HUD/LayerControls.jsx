034e2e9a89263502f6e2a19ab9dbdee0d2a70fee
Jeffrin James <jeffrinjamesworks@gmail.com>
2026-03-28 16:16:00 +0000

    react layer controls #301 unfinished

---
+++ src/components/HUD/LayerControls.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useMDASStore } from '../../stores/useMDASStore'
+import styles from './LayerControls.module.css'
+
+const LAYERS = [
+    { id: 'aircraft', label: 'CIVIL AIRCRAFT',}
+]
+
