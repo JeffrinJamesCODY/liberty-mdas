71092b66f1b1068addaed37dc45cc245898b53b1
Jeffrin James <jeffrinjamesworks@gmail.com>
2026-04-01 23:10:28 +0100

    updated App.jsx with added imports of feeds

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Globe from './components/Globe/Globe'
-import HUD from './components/HUD/HUD'
-import { useAircraftFeed } from './hooks/useAircraftFeed'
-import { useSatelliteFeed } from './hooks/useSatelliteFeed'
-import { useNEOFeed } from './hooks/useNEOFeed'
 import styles from './App.module.css'
+import Globe from './components/Globe/Globe.jsx'
+import HUD from './components/HUD/HUD.jsx'
+import { useAircraftFeed } from './hooks/useAircraftFeed.js'
+import { useNEOFeed } from './hooks/useNEOFeed.js'
+import { useSatelliteFeed } from './hooks/useSatelliteFeed.js'
 
 export default function App() {
   useAircraftFeed()
   useSatelliteFeed()
   useNEOFeed()
-  
+
   return (
     <div className={styles.app}>
-        <Globe />
-        <HUD />
-        </div>
+      <Globe />
+      <HUD />
+    </div>
   )
 }
